c6039195a2a0cc28d7b37183e3fc564feb3c584d
Joel Pilling <joel.pilling@gmail.com>
2015-01-04 17:56:26 -0600

    Log uploadloading and viewing works well enough, tests need to be written.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,9 +3,12 @@ class LogsController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
 
   def index
+    @logs = Log.all
   end
 
   def show
+    @log = Log.find(params[:id])
+    @log_text = File.read(Rails.root.join('public', 'logs', @log.path))
   end
 
   def new
@@ -13,6 +16,20 @@ class LogsController < ApplicationController
   end
 
   def create
+    @log = current_user.logs.build(log_params)
+    if params[:log][:log_file]
+      @log.path = "#{current_user.name}_#{Time.now.strftime('%F_%T')}.log"
+    else
+      @log.errors.add(:base, 'Must select a log to upload.')
+    end
+    if @log.save
+      File.open(Rails.root.join('public', 'logs', @log.path), 'wb') do |file|
+        file.write(params[:log][:log_file].read)
+      end
+      redirect_to logs_path, notice: 'Log successfully uploaded.'
+    else
+      render 'new'
+    end
   end
 
   def edit
