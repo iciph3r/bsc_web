cc399988b3854677c690374700f86beb888edf43
Joel Pilling <joel.pilling@gmail.com>
2015-01-09 16:39:12 -0600

    Messy commit, this cleans up the logs and topics show and index views.

--- app/helpers/log_controller_helper.rb
+++ app/helpers/log_controller_helper.rb
@@ -1,18 +1,26 @@
 module LogControllerHelper
 
   def colorize(line)
-    line.gsub!(/\*[[:alpha:]].+[[:alpha:]]\*/, '<span class="enemy">\0</span>')
+    line.gsub!(/\*[[:alpha:]]+\s.+[[:alpha:]]\*/, %Q[<span class="enemy">\\0</span>])
     case line
-    when /(^[[:alpha:]]+\snarrates\s'.+'$\n)/
-      %Q[<span class="yellow">#{$1}</span>]
-    when /(^.+\stells\syou\s'.+'$\n)/
-      %Q[<span class="green">#{$1}</span>]
-    when /(^.+\s(hits|cleaves|smites)\syou.+$\n)/
-      %Q[<span class="red">#{$1}</span>]
-    when /(^.+\ssays\s'.+'$\n)/, /(^You\s(hit|pound)\s.+$\n)/
-      %Q[<span class="cyan">#{$1}</span>]
-    when /(^.+\syells.+'.+'$\n)/
-      %Q[<span class="magenta">#{$1}</span>]
+    when /(^[[:alpha:]]+\snarrates\s'.+')(\r\n|\n|\r)/
+      %Q[<span class="yellow">#{$1}</span>#{$n}]
+    when /(^.+ tells you '.+')(\r\n|\n|\r)/
+      %Q[<span class="green">#{$1}</span>#{$n}]
+    when /(^.+ (hits|cleaves|smites|pierces|slashes|pounds|crushes|stabs|shoots) your .+)(\r\n|\n|\r)/,
+         /(^.+ sends you sprawling .+\.)(\r\n|\n|\r)/,
+         /(^You fall, and hit yourself!)(\r\n|\n|\r)/,
+         /(^You feel a sharp ache there!)(\r\n|\n|\r)/,
+         /(^That really HURT!)(\r\n|\n|\r)/
+      %Q[<span class="red">#{$1}</span>#{$n}]
+    when /(^.+ says '.+')(\r\n|\n|\r)/,
+         /(^You .*(slash|pierce|shoot|hit|pound|crush|smite|cleave|shoot).+)(\r\n|\n|\r)/,
+         /(^The lightning bolt hits .+impact\.)(\r\n|\n|\r)/
+      %Q[<span class="cyan">#{$1}</span>#{$n}]
+    when /(^.+ yells.+'.+')(\r\n|\n|\r)/
+      %Q[<span class="magenta">#{$1}</span>#{$n}]
+    when /(^(Heath|Brushy Valley|Gentle Slope|Ruins).+)(\r\n|\n|\r)/
+      %Q[<span class="green">#{$1}</span>#{$n}]
     else
       line
     end
